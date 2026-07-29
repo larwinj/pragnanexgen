1df7044e0a265ef849cbc8931abf310523bdbc48
Larwinj <119966720+larwinj@users.noreply.github.com>
2026-07-29 19:34:41 +0530

    Clean up Footer component by removing icons
    Removed unused social media icons from Footer component.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,5 @@
 import { Link } from 'react-router-dom'
-import {
-  Mail,
-  Phone,
-  MapPin,
-  Globe,
-  Linkedin,
-  Facebook,
-  Instagram,
-  Twitter,
-  ArrowRight,
-} from "lucide-react"
+import { Mail, Phone, MapPin, Globe, Linkedin, ArrowRight } from 'lucide-react'
 import Logo from './Logo'
 import { company, navLinks, products } from '../data/site'
 
@@ -44,7 +34,7 @@ export default function Footer() {
             >
             <Linkedin className="h-5 w-5" />
             </a>
-            <a
+            {/* <a
               href={company.social.facebook}
               target="_blank"
               rel="noreferrer noopener"
@@ -72,7 +62,7 @@ export default function Footer() {
                 aria-label="Twitter"
               >
               <Twitter className="h-5 w-5" />
-            </a>
+            </a> */}
           </div>
           </div>
 
